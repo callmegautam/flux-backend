e3b7f9c61b5777082c2ee524aac583c0ca8cb719
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-06-02 10:16:15 +0530

    Updated environment configuration to include AWS credentials and S3 bucket details.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const schema = z.object({
-    NODE_ENV: z.enum(['DEVELOPMENT', 'PRODUCTION']),
+    NODE_ENV: z.enum(["DEVELOPMENT", "PRODUCTION"]),
     PORT: z.coerce.number(),
     CORS_ORIGIN: z.string(),
     DB_URL: z.string(),
@@ -9,6 +9,10 @@ const schema = z.object({
     JWT_EXPIRY: z.string(),
     CLERK_PUBLISHABLE_KEY: z.string(),
     CLERK_SECRET_KEY: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
+    AWS_REGION: z.string(),
+    AWS_S3_BUCKET: z.string(),
 });
 
 export default schema.parse(process.env);
